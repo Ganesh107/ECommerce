bcd68e650a0f96ceefbabb9878d0abf75fa83b6e
Ganesh107 <sganesh371@gmail.com>
2024-11-24 18:29:04 +0530

    user registration db chnages

---
+++ app-backend/ECommerce.Database/ecom/Tables/User.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [ecom].[User] (
+    [UserId]       UNIQUEIDENTIFIER NOT NULL,
+    [Name]         NVARCHAR (128)   NOT NULL,
+    [Email]        NVARCHAR (MAX)   NOT NULL,
+    [PhoneNumber]  NVARCHAR (14)    NOT NULL,
+    [Hash]         VARBINARY (MAX)  NOT NULL,
+    [Salt]         VARBINARY (MAX)  NOT NULL,
+    [CreatedBy]    NVARCHAR (128)   NOT NULL,
+    [CreatedDate]  DATETIME         NOT NULL,
+    [ModifiedBy]   NVARCHAR (128)   NULL,
+    [ModifiedDate] DATETIME         NULL,
+    [IsActive]     BIT              DEFAULT (CONVERT([bit],(1))) NOT NULL,
+    CONSTRAINT [PK_User_UserId] PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
+
