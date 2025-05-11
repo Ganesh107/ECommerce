e822c815f1b98c057af0a5e20494e19b177edaa4
Ganesh107 <sganesh371@gmail.com>
2025-05-12 00:24:35 +0530

    navbar dynamic styles added

--- angular-frontend/src/app/navbar/navbar.component.ts
+++ angular-frontend/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
+  @ViewChild('nav') navContainer!: ElementRef;
+  showLeftButton: boolean = false;
+  
+  handleScroll(direction: string): void{
+    const container = this.navContainer.nativeElement;
+    const limit = direction == 'left' ? 200 : -200;
+    container.scrollBy({ left: limit, behavior: 'smooth' });
+    this.showLeftButton = direction === 'left' ? true : false;
+  }
+
 }
